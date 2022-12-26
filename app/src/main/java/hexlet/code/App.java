43b892cc1b61906b2017e878e6056d90f7558bbc
Bogdan <gannik120@gmail.com>
2022-12-26 21:50:28 +0300

    next step

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,25 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+import java.util.concurrent.Callable;
+@Command(
+        name = "genDif",
+        description = "Compares two configuration files and shows difference.",
+        mixinStandardHelpOptions = true,
+        version = "0.0.1"
+)
+public class App implements Callable<Integer> {
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        new CommandLine(new App()).execute(args);
+    }
+    @Override
+    public Integer call() throws Exception {
+        return 0;
     }
 }
+
