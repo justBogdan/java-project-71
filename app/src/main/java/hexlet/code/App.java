be38e6fcd1e6360a3a3d5f75a5af319f7b8446ac
Bogdan <gannik120@gmail.com>
2022-12-27 11:02:23 +0300

    next step

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -13,6 +13,13 @@ import java.util.concurrent.Callable;
         version = "0.0.1"
 )
 public class App implements Callable<Integer> {
+    @Parameters(index = "0", description = "path to first file")
+    private String filepath1;
+    @Parameters(index = "1", description = "path to second file")
+    private String filepath2;
+
+    @Option(names = {"-f", "--format"}, description = "output format [default: stylish]")
+    private String format;
 
     public static void main(String[] args) {
         new CommandLine(new App()).execute(args);
