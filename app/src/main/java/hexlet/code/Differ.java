844e4abc3d86bfc789d50d8864ab249705799b21
justBogdan <117362505+justBogdan@users.noreply.github.com>
2023-01-08 17:47:06 +0300

    Fixed logic
    fixed diff file

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,5 +1,9 @@
 package hexlet.code;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -8,26 +12,38 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Differ {
-    public static String generate(String path1, String path2) throws Exception {
+    public static String generate(String p1, String p2) throws Exception {
         ObjectMapper objectMap = new ObjectMapper();
-        Map<String, Object> fileValue1 = objectMap.readValue(path1, new TypeReference<Map<String, Object>>() {});
-        Map<String, Object> fileValue2 = objectMap.readValue(path2, new TypeReference<Map<String, Object>>() {});
+        ObjectMapper objectMap2 = new ObjectMapper();
 
-        Map mergedMap = Stream.concat(fileValue1.entrySet().stream(), fileValue2.entrySet().stream())
-                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+        Path path1 = Paths.get(p1).toAbsolutePath().normalize();
+        Path path2 = Paths.get(p2).toAbsolutePath().normalize();
+
+        String content1 = Files.readString(path1);
+        String content2 = Files.readString(path2);
+        Map<String, Object> fileValue1 = objectMap.readValue(content1, new TypeReference<Map<String, Object>>() {});
+        Map<String, Object> fileValue2 = objectMap2.readValue(content2, new TypeReference<Map<String, Object>>() {});
+
+        Map<String, Object> merge = new HashMap();
+        merge.putAll(fileValue1);
+        merge.putAll(fileValue2); // Вот здесь будет задача - правильно отсортировать пару для этого придется видимо поочерёдно добавлять
         String acc = "{\n";
-        for (var key : mergedMap.keySet()) {
-            var valueOfMerge = mergedMap.get(key);
-            var valueOfFirst = fileValue1.getOrDefault(key, null);
-            var valueOfSecond = fileValue2.getOrDefault(key, null);
-            if (!fileValue2.containsKey(key)) {
-                acc += String.format("- %s: %s\n", key, valueOfMerge);
+        for (String key : merge.keySet()) {
+            var valueMerge = merge.get(key);
+            var value1 = fileValue1.get(key);
+            var value2 = fileValue2.get(key);
+            if (!fileValue1.containsKey(key)) {
+                acc += String.format("+ %s: %s", key, valueMerge);
             }
-            else if (!fileValue1.containsKey(key)) {
-                acc += String.format("+ %s: %s\n", key, valueOfMerge);
+            else if (!fileValue2.containsKey(key)) {
+                acc += String.format("- %s: %s", key, valueMerge);
             }
-            else if (fileValue1.containsKey(key) && fileValue2.containsKey(key) &&) {
+            else if (!(value1.equals(value2))) {
+                acc += String.format("- %s: %s", key, value1);
+                acc += String.format("+ %s: %s", key, value2);
             }
         }
+        System.out.println("\n}");
+        return acc;
     }
 }
