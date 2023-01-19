e26d95eb3cd67f1eb197293c3568645641a4a94a
justBogdan <117362505+justBogdan@users.noreply.github.com>
2023-01-19 13:03:00 +0300

    update logic

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -3,7 +3,7 @@ import java.util.List;
 import java.util.Map;
 
 public class Differ {
-    public static String generate(String p1, String p2) throws Exception {
+    public static String generate(String p1, String p2, String format) throws Exception {
         List<Map<String, Object>> maps = FormatFactory.createMaps(p1, p2);
         var map1 = maps.get(0);
         var map2 = maps.get(1);
@@ -13,14 +13,14 @@ public class Differ {
             var value = map1.get(key);
             var value2 = map2.get(key);
             if (!map1.containsKey(key)) {
-                acc += Formatter.formatGenerate(key, value2, "+", App.format);
+                acc += Formatter.formatGenerate(key, value2, "+", format);
             } else if (!map2.containsKey(key)) {
-                acc += Formatter.formatGenerate(key, value, "-", App.format);
+                acc += Formatter.formatGenerate(key, value, "-", format);
             } else if (map1.get(key).equals(map2.get(key))) {
-                acc += Formatter.formatGenerate(key, value, " ", App.format);
+                acc += Formatter.formatGenerate(key, value, " ", format);
             } else if (!map1.get(key).equals(map2.get(key))) {
-                acc += Formatter.formatGenerate(key, value, "-", App.format);
-                acc += Formatter.formatGenerate(key, value2, "+", App.format);
+                acc += Formatter.formatGenerate(key, value, "-", format);
+                acc += Formatter.formatGenerate(key, value2, "+", format);
             }
         }
         acc += "}";
--- app/src/test/java/hexlet/code/DifferTest2.java
+++ app/src/test/java/hexlet/code/DifferTest2.java
@@ -6,62 +6,31 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 public class DifferTest2 {
     @Test
-    void testRelativePathJSON() throws Exception {
-        String subStr1 = "+ age: 40";
-        String subStr2 = "- car: BMW";
-        String subStr3 = "+ car: Pagani";
-        var f = "./src/test/resources/JSONFiles/testFile1.json";
-        var s = "./src/test/resources/JSONFiles/testFile2.json";
-        String answer = Differ.generate(f, s);
-        boolean expected = true;
-        boolean actual1 = answer.contains(subStr1);
-        boolean actual2 = answer.contains(subStr2);
-        boolean actual3 = answer.contains(subStr3);
-        assertEquals(expected, actual1);
-        assertEquals(expected, actual2);
-        assertEquals(expected, actual3);
-    }
-    @Test
-    void testDefunctPath() {
-        Throwable thrown = assertThrows(IOException.class, () -> {
-            var f = "./src/test/resources/JSONFile/testFile12.json";
-            var s = "./src/test/resources/JSONFile/testFile22.json";
-            var answer = Differ.generate(f, s);
-        });
-        assertNotNull(thrown.getMessage());
-    }
-    @Test
-    void testIfFileEmpty() {
-        Throwable thrown = assertThrows(IOException.class, () -> {
-            var f = "./src/test/resources/JSONFiles/testEmptyFile1.json";
-            var s = "./src/test/resources/JSONFiles/testEmptyFile2.json";
-            var answer = Differ.generate(f, s);
-        });
-        assertNotNull(thrown.getMessage());
-    }
-    @Test
      void testRelativePathYAML() throws Exception {
         var f = "./src/test/resources/YAMLFiles/testFile1.yaml";
         var s = "./src/test/resources/YAMLFiles/testFile2.yaml";
-        var answer = Differ.generate(f, s);
-        var param2 = "- CustomerName: Joe";
-        var param3 = "+ CustomerName: Victoria";
-        var param4 = "+ unitPrice: 100";
-        var param5 = "- unitPrice: 12";
+        var answer = Differ.generate(f, s, "stylish");
+        var param2 = "  - CustomerName: Joe";
+        var param3 = "  + CustomerName: Victoria";
+        var param4 = "  + unitPrice: 100";
+        var param5 = "  - unitPrice: 12";
         assertEquals(true, answer.contains(param2));
         assertEquals(true, answer.contains(param3));
         assertEquals(true, answer.contains(param4));
         assertEquals(true, answer.contains(param5));
     }
     @Test
-    void testIfKeyIsObj() throws  Exception {
+    void testIfKeyIsObj() throws Exception {
         var f = "./src/test/resources/YAMLFiles/testFile3.yaml";
         var s = "./src/test/resources/YAMLFiles/testFile4.yaml";
-        var answer = Differ.generate(f, s);
-        var param0 = "  - setting1: Some Value";
-        var param1 = "  + setting1: Another Value";
-        var param2 = "  - numbers: [1, 2, 3, 4]";
-        var param3 = "  + numbers: [10, 9, 8]";
-
+        var answer = Differ.generate(f, s, "stylish");
+        var param6 = "  - setting1: Some Value";
+        var param7 = "  + setting1: Another Value";
+        var param8 = "  - numbers1: [1, 2, 3, 4]";
+        var param9 = "  + numbers1: [10, 9, 8]";
+        assertEquals(true, answer.contains(param6));
+        assertEquals(true, answer.contains(param7));
+        assertEquals(true, answer.contains(param8));
+        assertEquals(true, answer.contains(param9));
     }
 }
