685223ead733ae84bc3749f0c94469a079fee8e7
justBogdan <117362505+justBogdan@users.noreply.github.com>
2023-01-19 13:07:14 +0300

    Update DifferTest2.java

--- app/src/test/java/hexlet/code/DifferTest2.java
+++ app/src/test/java/hexlet/code/DifferTest2.java
@@ -1,9 +1,6 @@
 package hexlet.code;
 import org.junit.jupiter.api.Test;
-import java.io.IOException;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 public class DifferTest2 {
     @Test
      void testRelativePathYAML() throws Exception {
